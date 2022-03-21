1282e5c5162a85e49006a4a1a1e508425d126461
Lionovich <lionovich73@gmail.com>
2022-03-22 01:05:35 +0300

    Домашнее задание 1

---
+++ JavaHomework1/HomeworkApp.java
@@ -0,0 +1,29 @@
+public class HomeworkApp {
+    public static void main(String[] args) {
+        PrintThreeWords();
+        checkSumSign();
+        printColor();
+    }
+    public static void PrintThreeWords() {
+        System.out.println("Apple");
+        System.out.println("Orange");
+        System.out.println("Banana");
+    }
+    public static void checkSumSign() {
+        int a = 15;
+        int b = 6;
+        if (a+b >= 0)
+            System.out.println("Positive");
+        else
+            System.out.println("Negative");
+    }
+    public static void printColor() {
+        int value = 2543;
+        if (value <= 0) 
+            System.out.println("Red");
+        if (value > 100)
+            System.out.println("Green");
+        else
+            System.out.println("Yellow");
+    }
+}
