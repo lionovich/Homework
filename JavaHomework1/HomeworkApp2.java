8273f2aafdc535f84e5b6315b00939340e6e6890
Lionovich <lionovich73@gmail.com>
2022-03-28 10:32:50 +0300

    оптимизировал задание про високосный год

--- JavaHomework1/HomeworkApp2.java
+++ JavaHomework1/HomeworkApp2.java
@@ -49,11 +49,6 @@ static void iterator(String text, int e) {
 }
 
 static boolean leapYear(int y) {
-    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0);
 }
 }
