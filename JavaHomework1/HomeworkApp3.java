fafa3ae709d6e0b80e3dc3fa173e0a6aee9632c0
Lionovich <lionovich73@gmail.com>
2022-04-07 23:52:04 +0300

    1

--- JavaHomework1/HomeworkApp3.java
+++ JavaHomework1/HomeworkApp3.java
@@ -4,6 +4,14 @@
  * @date 07.04.2022
  */
 
+import java.util.Arrays;
+
  public class HomeworkApp3 {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}; {
+        for (int i = 0; i < arr.length; i++) {
+        }
+            System.out.println(Arrays.toString(arr));
+        }
+    }
 }
